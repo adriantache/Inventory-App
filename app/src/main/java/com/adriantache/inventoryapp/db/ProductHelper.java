d0e04f2ae9802d9ec9e0214b11eed5f3c17c2fbb
Adrian Tache <contact@adriantache.com>
2018-05-03 20:24:08 +0300

    Add DB Helper class

---
+++ app/src/main/java/com/adriantache/inventoryapp/db/ProductHelper.java
@@ -0,0 +1,38 @@
+package com.adriantache.inventoryapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.adriantache.inventoryapp.db.ProductContract.ProductEntry.*;
+
+/**
+ * Created by elabi3 on 03/05/2018.
+ **/
+public class ProductHelper extends SQLiteOpenHelper {
+    private final static String DATABASE_NAME = "inventory.db";
+    private final static int DATABASE_VERSION = 1;
+
+    public ProductHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE = "CREATE TABLE "+TABLE_NAME +
+                " (" + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "+
+                COLUMN_PRICE + " INTEGER NOT NULL, "+
+                COLUMN_QUANTITY + " INTEGER NOT NULL, "+
+                COLUMN_SUPPLIER_NAME + " TEXT NOT NULL, "+
+                COLUMN_SUPPLIER_PHONE+ " TEXT);";
+        db.execSQL(SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        final String SQL_DESTROY = "DROP TABLE IF EXISTS"+TABLE_NAME+";";
+        db.execSQL(SQL_DESTROY);
+        onCreate(db);
+    }
+}
